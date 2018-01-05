3ba80c9d580b70a2b7b2f9e7c3cefba3108a18c4
Neal Fultz <nfultz@gmail.com>
2018-01-05 00:37:24 +0000

    Update list_R_files.R
    Small refactor while I'm reading your code.

--- R/list_R_files.R
+++ R/list_R_files.R
@@ -3,27 +3,19 @@
 #' @param path A character string indicating a path or a list of paths
 #' 
 #' @return A character vector of found files with extensions in ".R"
-#' @importFrom dplyr %>%
 #' @export
 list_R_files = function(path = ".") {
   
   if (!is.character(path)) {
     stop("Provided path(s) must be character(s)")
   }
-  
-  # Check which path exists
-  path_exist = sapply(path, dir.exists)
-  
-  path = path[path_exist]
+  path = Filter(dir.exists, path)
   
   if (length(path) == 0) {
     stop("Directory does not exist")
   }
   
-  files_list = list.files(path) %>%
-    # Get only files ending in ".R"
-    stringr::str_extract_all(pattern = ".*.R$") %>%
-    unlist()
+  files_list = list.files(path, pattern="[.]R$")
   
   return(files_list)
 }
